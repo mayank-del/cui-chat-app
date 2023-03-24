8cfe77e4c48e31619a4da009807f6dcd190c59d2
mayank-del <mkjam.007@gmail.com>
2023-03-25 00:50:00 +0530

    nnew file

--- server/index.js
+++ server/index.js
@@ -1,22 +1,56 @@
-const cors=require("cors")
-const io=require("socket.io")(8000,{cors:{origin:"*"}})
+const express = require('express');
+const app = express();
 
-const users={};
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+// Create an in-memory database
+const users = [];
 
-io.on("connection",socket=>{
-    socket.on('new-user-joined',name=>{
-        //console.log("user:",name);
-        users[socket.id]=name;
-        socket.broadcast.emit('user-joined',name);
-    });
+// Get all users
+app.get('/users', (req, res) => {
+  res.json(users);
+});
 
-    socket.on('send',message=>{
-        socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
-    });
+// Get a specific user by ID
+app.get('/users/:id', (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (!user) return res.status(404).send('User not found');
+  res.json(user);
+});
 
-    socket.on('disconnect',message=>{
-        socket.broadcast.emit('left',users[socket.id]);
-        delete users[socket.id];
-    });
+// Add a new user
+app.post('/users', (req, res) => {
+  const user = {
+    id: users.length + 1,
+    name: req.body.name,
+    email: req.body.email
+  };
+  users.push(user);
+  res.status(201).send(user);
+});
 
-})
+// Update a user
+app.put('/users/:id', (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (!user) return res.status(404).send('User not found');
+
+  user.name = req.body.name;
+  user.email = req.body.email;
+
+  res.json(user);
+});
+
+// Delete a user
+app.delete('/users/:id', (req, res) => {
+  const index = users.findIndex(u => u.id === parseInt(req.params.id));
+  if (index === -1) return res.status(404).send('User not found');
+
+  users.splice(index, 1);
+
+  res.json({ message: 'User deleted' });
+});
+
+
+// Start the server
+const PORT = 8000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
